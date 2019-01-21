7ef20f4455b3349d795f41d6d7663b1c5e68e276
Ben Johns <benjohns.git.gpg.donotreply@gmail.com>
2019-01-21 15:24:46 -0700

    chi-todo-api

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 golangbot-tutorial: https://golangbot.com/learn-golang-series/
 
-build-web-app: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/index.html
+astaxie-web-app: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/index.html
+
+chi-api: https://itnext.io/structuring-a-production-grade-rest-api-in-golang-c0229b3feedc
---
+++ chi-api/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"golang-tutorials/chi-api/todo"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+func routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.DefaultCompress,
+		middleware.RedirectSlashes,
+		middleware.Recoverer,
+	)
+
+	router.Route("/v1", func(r chi.Router) {
+		r.Mount("/api/todo", todo.Routes())
+	})
+
+	return router
+}
+
+func main() {
+	router := routes()
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("logging err: %s\n", err.Error())
+	}
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
